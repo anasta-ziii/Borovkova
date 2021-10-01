c857c2f06eec89c7b43fe6a86f499817b0ee44a0
Borovkova AD <90901815+anasta-ziii@users.noreply.github.com>
2021-10-01 13:10:33 +0300

    Add files via upload

---
+++ Задание 3/Задача 1.py
@@ -0,0 +1,7 @@
+﻿A = int(input("Введите первое число:"))
+B = int(input("Введите второе число:"))
+if A>B:
+ for i in range(A - (A + 1) % 2, B - B % 2, -2):
+    print(i, end=' ')
+else:
+  print("ошибка!")
---
+++ Задание 3/Задача 4.py
@@ -0,0 +1,6 @@
+s=0
+n=int(input("Введите кол-во переменных:"))
+for i in range(n):
+    x=int(input())
+    s+=x
+print(s)
