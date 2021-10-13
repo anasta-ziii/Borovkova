76c5e09313a95fd304904503b1917b73ebd1e543
Borovkova AD <90901815+anasta-ziii@users.noreply.github.com>
2021-10-14 02:21:47 +0300

    Delete the third lesson directory

--- the third lesson/Задача 3.py
+++
@@ -1,7 +0,0 @@
-﻿A = int(input("Введите первое число A:"))
-B = int(input("Введите второе число B, которое меньше А:"))
-if A>B:
- for i in range(A - (A + 1) % 2, B - B % 2, -2):
-    print(i)
-else:
-  print("ошибка!")
--- the third lesson/Задача 4.py
+++
@@ -1,6 +0,0 @@
-s=0
-n=int(input("Введите кол-во переменных:"))
-for i in range(n):
-    x=int(input("Введите число:"))
-    s+=x
-print(s)
