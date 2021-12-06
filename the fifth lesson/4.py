7f17c184bd1f81cd5d87403566d2f664d0875b7f
Borovkova AD <90901815+anasta-ziii@users.noreply.github.com>
2021-12-07 01:41:48 +0300

    Add files via upload

---
+++ the fifth lesson/4.py
@@ -0,0 +1,10 @@
+# -- coding: utf-8 --
+def m4():
+ x = int(input("Введите первое число:"))
+ y = int(input("Введите второе число:"))
+ counter = 1
+ while x < y:
+  x *= 1,1
+  counter+=1
+ print(counter)
+m4() 
---
+++ the fifth lesson/5.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+print('Задание #5')
+def m():
+    a = 0
+    while int(input("Введите число:")) != 0:
+        a+=1
+    print(a)
+m()
